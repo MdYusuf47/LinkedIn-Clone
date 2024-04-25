a3c581a8efe553aa29cc1310daac8c57fbeb82c4
Md Yusuf <mdyusuf742171@gmail.com>
2024-04-25 22:07:56 +0530

    setup project and create header componet

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>LinkedIn Clone</h1>
+ 
+    </main>
+  );
+}
