f6078599b72d0ca9e0b1ebdd0f2759f035d7dcf0
Md Yusuf <mdyusuf742171@gmail.com>
2024-04-30 22:30:58 +0530

    setup sign in and sign out functionality using clerk

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import {
   Briefcase,
   HomeIcon,
@@ -8,6 +9,7 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
@@ -48,6 +50,17 @@ const Header = () => {
           <MessagesSquare className="h-5" />
           <p>Messaging</p>
         </Link>
+
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
+
+      <SignedOut>
+        <Button asChild variant="secondary">
+          <SignInButton/>
+        </Button>
+      </SignedOut>
+
       </div>
     </div>
   );
