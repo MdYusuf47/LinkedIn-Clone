0cfd6be2dedd22740b510e04f0ebdc18eb989660
Md Yusuf <mdyusuf742171@gmail.com>
2024-05-03 11:06:48 +0530

    Create side user information

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import UserInformation from "@/components/UserInformation";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>LinkedIn Clone</h1>
- 
-    </main>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline md:col-span-2">{/* user information */}
+        <UserInformation/>
+      </section>
+
+      <section>
+        {/* postform */}
+        {/* postfeed */}
+      </section>
+
+      <section>{/* wedget */}</section>
+    </div>
   );
 }
